cc6d41d33647df0b076f73b0481051b2d473a5c1
s-light <github@s-light.eu>
2019-01-15 16:11:05 +0100

    add id as link focus

--- fix_css.js
+++ fix_css.js
@@ -25,6 +25,7 @@ function add_table_wrapper() {
     if (table) {
         const el_wrapper = document.createElement('div');
         el_wrapper.classList.add('table_wrapper');
+        el_wrapper.id = 'table_focus';
         // move table as child to wrapper..
         table.parentElement.appendChild(el_wrapper);
         el_wrapper.appendChild(table);
